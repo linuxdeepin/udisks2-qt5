8ba940cd0f8722731bdef5b7fe4ce5b2a6d75468
liuzhangjian <liuzhangjian@uniontech.com>
2022-09-22 10:32:54 +0800

    chore: adapt REUSE license
    修改开源合规
    
    Log: adapt REUSE license

--- private/dblockdevice_p.h
+++ private/dblockdevice_p.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2020 - 2022 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef DBLOCKDEVICE_P_H
 #define DBLOCKDEVICE_P_H
